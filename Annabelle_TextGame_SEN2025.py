b584ed67e167b4e3107cb87d9d40639f25ccde26
AnnabelleLogan <138072365+AnnabelleLogan@users.noreply.github.com>
2025-06-16 01:41:07 +1000

    Update Annabelle_TextGame_SEN2025.py

--- Annabelle_TextGame_SEN2025.py
+++ Annabelle_TextGame_SEN2025.py
@@ -193,20 +193,240 @@ if information == "Yes":
             print(" ")
             typing_effect("Mission:", delay=0.1)
             typing_effect(" Exterminate the Rock Lizard queen", delay=0.1)
+            type_effect("you step off your ship, finally landing of the planet-like asteroid", delay=0.2)
 
-class Aesteroid():
+
+
+
+class Enemy(Character):
+    def __init__(self, char_name, char_description):
+        super().__init__(char_name, char_description)
+        harry = Enemy("Lizard Queen", "The source of the plague")
+
+class Enemy(Character):
+    def __init__(self, char_name, char_description):
+        super().__init__(char_name, char_description)
+        self.weakness = None
+
+   
+    def fight(self, combat_item):
+if combat_item == self.weakness:
+    print("You fend " + self.name + " off with the " + combat_item )
+    return True
+else:
+    print(self.name + " swallows you, little wimp")
+    return False
+
+harry.set_weakness("Electricity")
+
+print("What will you fight with?")
+fight_with = input()
+harry.fight(fight_with)
+
+
+class Asteroid:
     def __init__(self, Planet_name):
         self.name = Planet_name
         self.description = None
+        self.linked_locations = {}
+        self.character = None
+        
+current_locations = Plains
+dead = False
+while dead == False:
+
+print("\n")         
+    current_cave.get_details()
+    # Add your code here
+    command = input("> ")    
+    current_cave = current_cave.move(command)
+
+inhabitant = current_locations.get_character()
+if inhabitant is not None:
+    inhabitant.describe()
+command = input("<")
+current_locations = current_locations.move(command)
+
+elif command == "talk":
+        if inhabitant is not None:
+            inhabitant.talk()
+
+elif command == "fight":
+        if inhabitant is not None and isinstance(inhabitant, Enemy):
+            # Fight with the inhabitant, if there is one
+            print("What will you fight with?")
+            fight_with = input()
+            if inhabitant.fight(fight_with) == True:
+                # What happens if you win?
+                print("Bravo,hero you won the fight!")
+                current_room.set_character(None)
+            else:
+                print("Scurry back to your ship coward, you lost the fight.")
+        else:
+            print("There is no one here to fight with")
+
+elif command == "fight":
+        ...
+        if inhabitant.fight(fight_with) == True:
+        else:
+            typing_effect("Run away twerp, you lost this fight.",delay=0.2)
+            typing_effect("That's the end of the game",delay=0.2)
+            dead = True
+
+from character import Character
+
+class Enemy(Character):
+    ...  
+    def steal(self):
+        print("You steal from " + self.name)
+
+class Enemy(Character):
+    ...  
+    def steal(self):
+        print("You steal from " + self.name)
+
+class Friend(Character):
+    def __init__(self, char_name, char_description):
+        super().__init__(char_name, char_description)
+        self.feeling = None
+    def pat(self):
+        print(self.name + " pats you back!")
+
+harry = Enemy("Lizard Queen", "The source of the plague")
+harry.set_conversation("It's too late to stop it now, you could never win")
+harry.set_weakness("electricity")
+Mountain.set_character(Lizard Queen)
+Lesly = Friend("Josephine", "A friendly Lizard")
+Lesly.set_conversation("Hey!")
+Plains.set_character(Lesly)
+
+elif command == “pat”:
+        if inhabitant is not None:
+            if instance(inhabitant, Enemy):
+                print(“I wouldn’t do that if I were you…")
+            else:
+                inhabitant.pat()
+        else:
+            print("There is no one here to pat :(")
+
+class Asteroid:
+    def __init__(self, Planet_name):
+        ...
+        self.item = None
+def get_item(self):
+    return self.item
+def set_item(self, item_name):
+    self.item = item_name
+
+def describe(self):
+    print ("The [" + self.name + "] is here - " + self.description)
+
+vegemite = Item("electricity")
+vegemite.set_description("The Lizard Queen's weakness")
+grotto.set_item(electricity)
+torch = Item("walkie talkie")
+torch.set_description("A communication device")
+dungeon.set_item(Walkie Talkie)
+
+while dead == False:
+    item = current_cave.get_item()
+    if item is not None:
+        item.describe()
+
+Class Item():
+    def __init__(self, item_name):
+        self.name = item_name
+        self.description = None
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, item_name):
+        self.name = item.name
 
     def get_description(self):
         return self.description
 
-    def set_description(self, Asteroid_description):
-        self.description = Asteroid_description
+    def set_description(self, item_description):
+        self.description = item.description
 
-    def set_description(self, Asteroid_description):
-        self.description = Asteroid_description
+    def describe(self):
+        print(
+            "The I" + self.name + "I is here - " + delf.description
+            )
+
+    torch = Item("Walkie Talkie")
+    torch.set_description("A communication device")
+    dungeon.set_item(Walkie Talkie)
+    bag = []
+
+elif command == "take":
+    if item is not None:
+      print("You put the " + item.get_name() + " in your bag")
+      bag.append(item.get_name())
+      current_room.set_item(None)
+
+elif command == "fight":
+    if inhabitant is not None and instance(inhabitant, Enemy):
+        print("What will you fight with?")
+        fight_with = input()
+        if fight_with in bag:
+            if inhabitant.fight(fight_with) == True:
+                print("Bravo hero you have won the fight!")
+                current_locations.self_character(None)
+                if Enemies.enemies_to_defeat == 0
+                        print("Congradulations! You have survived yet another adventure")
+                        dead = True
+            else:
+                print("Scurry back to the rocket, you lost the fight")
+                print("That's the end of your game")
+                dead = True
+        else:
+            print("You dont have a " + fight_with)
+    else:
+        print("There is no one here to fight with")
+
+
+class Enemy(Character):
+    enemies_to_defeat = 0
+    def __init__(self, char_name, char_description):
+        Enemy.enemies_to_defeat = Enemy.enemies_to_defeat + 1
+
+class Enemy(Character):
+    def fight(self, combat_item):
+        if combat_item == self.weakness:
+            Enemy.enemies_to_defeat = Enemy.enemies_to_defeat - 1
+            return True
+
+    enemies_to_defeat = 0
+    def __init__(self, char_name, char_description):
+        super().__init__(self, char_name, char_description):
+        self.weakness = None
+        Enemy.enemies_to_defeat = Enemy.enemies_to_defeat + 1
+
+    def set_weakness(self, item_weakness):
+        self.weakness = item_weakness
+
+    def get_weakness(self)
+        return erlf.weakness
+
+    def fight(self, combat_item):
+        if combat_item == self.weakness:
+            print("You fend " + self.name + " off with the " combat_item)
+            Enemy.enemies_to_defeat = Enemy.enemies_to_defeat - 1
+            Return True
+        else:
+            print{self.name + " swallowss you, you little wimp")
+            Return False
+
+    def Steal(self)
+        print("You steal from " + self.name)
+
+elif command == "fight":
+            if inhabitant.fight(fight_with) == True:
+                if Enemy.enemies_to_defeat == 0:
+                    print("Congratulations, you have survived another adventure!")
+                    dead = True
 
 
 
