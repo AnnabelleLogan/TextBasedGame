eea387d94954f0b8fef738aab5afed24a0c1ce4a
AnnabelleLogan <138072365+AnnabelleLogan@users.noreply.github.com>
2025-06-15 23:08:35 +1000

    Update Asteroid.py

--- Asteroid.py
+++ Asteroid.py
@@ -2,11 +2,17 @@ class Asteroid():
     def __init__(self, Planet_name):
         self.name = Planet_name
         self.description = None
-
     def get_description(self, Planet_name):
         return self.description
+    def describe(self):
+        print( self.description )
 
-describe()
+    def __init__(self, Planet_name):
+        self.linked_caves = {}
+
+    
+
+    
 
 
 
