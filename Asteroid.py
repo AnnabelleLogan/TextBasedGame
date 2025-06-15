a6b0e5182cce81ea80d315c81441025a305b3c6b
AnnabelleLogan <138072365+AnnabelleLogan@users.noreply.github.com>
2025-06-15 23:02:23 +1000

    Update and rename cave.py to Asteroid.py

---
+++ Asteroid.py
@@ -0,0 +1,13 @@
+class Asteroid():
+    def __init__(self, Planet_name):
+        self.name = Planet_name
+        self.description = None
+
+    def get_description(self, Planet_name):
+        return self.description
+
+describe()
+
+
+
+    
--- cave.py
+++
@@ -1,9 +0,0 @@
-class Asteroid:
-    def __init__(self, planet_name):
-        self.name = cave_name
-        self.description = None
-
-    def get_description(self, planet_name):
-        return self.description
-
-    
