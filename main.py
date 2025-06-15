e7ccb8250c391d4e9ff25804e35ca0ba1b9ca8fc
AnnabelleLogan <138072365+AnnabelleLogan@users.noreply.github.com>
2025-06-15 22:49:20 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 from asteroid import Asteroid
+
+Plains = Asteroid("Plains")
+Rocket_Pad = Asteroid("Rocket Pad")
+Research_Facility = Astesroid("Research Facility")
+Water_Banks = Asteroid("Water Banks")
+Mountain = Asteroid("Mountain")
+Forested_Area = Asteroid("Forested Area")
+
