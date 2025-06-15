ec710e7e085a05261457c10695ba4867cc67d464
AnnabelleLogan <138072365+AnnabelleLogan@users.noreply.github.com>
2025-06-15 22:40:08 +1000

    Update cave.py

--- cave.py
+++ cave.py
@@ -1,9 +1,9 @@
-class Cave:
-    def __init__(self, cave_name):
+class Asteroid:
+    def __init__(self, planet_name):
         self.name = cave_name
         self.description = None
 
-    def get_description(self, cave_description):
+    def get_description(self, planet_name):
         return self.description
 
     
