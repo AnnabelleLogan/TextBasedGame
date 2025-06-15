c5e52659e358c39bd020b17ea067580d90acf7f8
AnnabelleLogan <138072365+AnnabelleLogan@users.noreply.github.com>
2025-06-16 00:11:55 +1000

    Update Test_Character.py

--- Test_Character.py
+++ Test_Character.py
@@ -1,5 +1,20 @@
 from Character import = Character():
 harry = Character("Harry", "A smelly Wumpus")
 harry.describe{}
+harry.set_conversation("Come closer, ! can't see you!")
+harry.talk()
 
 
+
+
+
+
+
+#class Character():
+
+#  def set_conversation(self, conversation):
+#        self.conversation = conversation
+#harry.set_conversation ("Come closer.I can’t see you!")
+
+
+  
